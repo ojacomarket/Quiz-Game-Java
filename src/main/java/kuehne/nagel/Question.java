dd20c052c93adbed97018be385474a3b593f9633
ojacomarket <elfutbolesmivida@mail.ru>
2021-10-02 18:52:48 +0300

    Add constructor for querying question by topic, SHORTEN BY LOMBOK

--- src/main/java/kuehne/nagel/Question.java
+++ src/main/java/kuehne/nagel/Question.java
@@ -10,17 +10,10 @@ public class Question {
 
     private int id;
     @NonNull
+    private String topic_name;
+    @NonNull
     private String content;
     @NonNull
     private int difficulty;
     private int topic_id;
-    @NonNull
-    private String topic_name;
-
-    public Question (String topic_name, String content, int difficulty) {
-        this.topic_name = topic_name;
-        this.content = content;
-        this.difficulty = difficulty;
-        this.topic_id = topic_id;
-    }
 }
