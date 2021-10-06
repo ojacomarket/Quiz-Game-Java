8e3971e6d485e6eb6f91132ccfe42411ce794aa7
ojacomarket <elfutbolesmivida@mail.ru>
2021-10-06 23:09:15 +0300

    SEARCH:test4 FINISH TESTING SEARCH QUERIES

--- src/test/java/kuehne/nagel/dao/DaoQuestionTest.java
+++ src/test/java/kuehne/nagel/dao/DaoQuestionTest.java
@@ -2,11 +2,8 @@ package kuehne.nagel.dao;
 
 import kuehne.nagel.Question;
 import kuehne.nagel.Response;
-import kuehne.nagel.utils.SaveQuestion;
-import kuehne.nagel.utils.SearchQuestionByTopic;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -14,10 +11,10 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 
- class DaoQuestionTest {
-    public static final List<Question> EMPTY_LIST = new LinkedList <> ();
-    public static final List<Question> LIST_TO_BE_FILLED = new LinkedList <> ();
-    public static final List<Response> LIST_TO_BE_FILLED2 = new LinkedList <> ();
+class DaoQuestionTest {
+    public static final List<Question> EMPTY_LIST = new LinkedList<>();
+    public static final List<Question> LIST_TO_BE_FILLED = new LinkedList<>();
+    public static final List<Response> LIST_TO_BE_FILLED2 = new LinkedList<>();
     public static final Question CARS1 = new Question("Cars", "What is the fastest car on a planet?", 4);
     public static final Question CARS2 = new Question("Cars", "What is the first car invented?", 5);
     public static final Question CARS3 = new Question("Cars", "Which car has a horse as its logo?", 1);
@@ -28,23 +25,33 @@ import static org.junit.jupiter.api.Assertions.*;
     @DisplayName("PART 1: Search question by topic: TEST 1: If we input empty string on topic query" +
             " we will get empty list")
     void no_topic_return_empty_list() {
-        assertEquals(EMPTY_LIST, interactWithDb.searchQuestion(""));
+        assertEquals(EMPTY_LIST, interactWithDb.showAllQuestionsByTopic(""));
     }
-     @Test
-     @DisplayName("PART 1: Search question by topic: TEST 2: If topic doesn't exist we have nothing to do" +
-             " with questions, since they simply don't exist either")
-     void if_topic_doesnt_exist_return_empty_list() {
-         assertEquals(EMPTY_LIST, interactWithDb.searchQuestion("3rf3rg"));
-     }
-     @Test
-     @DisplayName("PART 1: Search question by topic: TEST 3: If topic does exist we get data from it," +
-             " namely topic name, content of question and difficulty level")
-     void if_topic_does_exist_return_list_with_topic_question_and_difficulty_lvl() {
-         LIST_TO_BE_FILLED.add(CARS1);
-         LIST_TO_BE_FILLED.add(CARS2);
-         LIST_TO_BE_FILLED.add(CARS3);
-         assertEquals(LIST_TO_BE_FILLED, interactWithDb.searchQuestion("Cars"));
-     }
+
+    @Test
+    @DisplayName("PART 1: Search question by topic: TEST 2: If topic doesn't exist we have nothing to do" +
+            " with questions, since they simply don't exist either")
+    void if_topic_doesnt_exist_return_empty_list() {
+        assertEquals(EMPTY_LIST, interactWithDb.showAllQuestionsByTopic("3rf3rg"));
+    }
+
+    @Test
+    @DisplayName("PART 1: Search question by topic: TEST 3: If topic doesn't exist we cannot get data," +
+            " from it, so we return empty list")
+    void if_table_doesnt_exist_return_empty_list() {
+        assertEquals(EMPTY_LIST, interactWithDb.showAllQuestionsByTopic("Cars"));
+    }
+
+    @Test
+    @DisplayName("PART 1: Search question by topic: TEST 4: If topic does exist we get data from it," +
+            " namely topic name, content of question and difficulty level")
+    void if_topic_does_exist_return_list_with_topic_question_and_difficulty_lvl() {
+        LIST_TO_BE_FILLED.add(CARS1);
+        LIST_TO_BE_FILLED.add(CARS2);
+        LIST_TO_BE_FILLED.add(CARS3);
+        assertEquals(LIST_TO_BE_FILLED, interactWithDb.showAllQuestionsByTopic("Cars"));
+    }
+
     /* @Test
      @DisplayName("PART 2: Save question: TEST 1: If we input empty string on saving query" +
              " we will get 0")
@@ -54,7 +61,7 @@ import static org.junit.jupiter.api.Assertions.*;
          LIST_TO_BE_FILLED2.add(new Response("Bugatti"));
          assertEquals(0, saveQuestion.saveQuestion("","What is the fastest car on a planet?",5,LIST_TO_BE_FILLED2));
      }
-     @Test
+    /* @Test
      @DisplayName("PART 2: Save question: TEST 2: If we input empty question content on saving query "+
              "we will get 0")
      void no_question_on_question_returns_0() {
