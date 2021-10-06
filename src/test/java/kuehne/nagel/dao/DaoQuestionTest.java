dbe11140c795406c179530e2440ff13ce1b770e9
ojacomarket <elfutbolesmivida@mail.ru>
2021-10-06 22:47:33 +0300

    SEARCH:test2

--- src/test/java/kuehne/nagel/dao/DaoQuestionTest.java
+++ src/test/java/kuehne/nagel/dao/DaoQuestionTest.java
@@ -30,14 +30,13 @@ import static org.junit.jupiter.api.Assertions.*;
     void no_topic_return_empty_list() {
         assertEquals(EMPTY_LIST, interactWithDb.searchQuestion(""));
     }
-  /*   @Test
+     @Test
      @DisplayName("PART 1: Search question by topic: TEST 2: If topic doesn't exist we have nothing to do" +
              " with questions, since they simply don't exist either")
      void if_topic_doesnt_exist_return_empty_list() {
-         SearchQuestionByTopic new_search = new SearchQuestionByTopic();
-         assertEquals(EMPTY_LIST, new_search.searchQuestionByTopic("3rf3rg"));
+         assertEquals(EMPTY_LIST, interactWithDb.searchQuestion("3rf3rg"));
      }
-     @Test
+    /* @Test
      @DisplayName("PART 1: Search question by topic: TEST 3: If topic does exist we get data from it," +
              " namely topic name, content of question and difficulty level")
      void if_topic_does_exist_return_list_with_topic_question_and_difficulty_lvl() {
