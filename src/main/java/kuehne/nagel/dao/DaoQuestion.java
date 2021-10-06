10ae39983320651ecc2e57a1ddcef8d5013221ce
ojacomarket <elfutbolesmivida@mail.ru>
2021-10-06 19:11:02 +0300

    Add fresh data

--- src/main/java/kuehne/nagel/dao/DaoQuestion.java
+++ src/main/java/kuehne/nagel/dao/DaoQuestion.java
@@ -2,10 +2,7 @@ package kuehne.nagel.dao;
 
 import kuehne.nagel.Question;
 import kuehne.nagel.Response;
-import kuehne.nagel.utils.GetUnusedId;
-import kuehne.nagel.utils.SearchQuestionIDbyQuestionName;
-import kuehne.nagel.utils.SearchTopicIDbyTopicName;
-import kuehne.nagel.utils.VerifyQuestionDuplicate;
+import kuehne.nagel.utils.*;
 import lombok.Data;
 
 import java.sql.*;
@@ -24,6 +21,10 @@ public class DaoQuestion extends DatabaseConnection implements DaoQuestionI {
     private static final String SQL_QUERY_RETRIEVE_UNUSED_ID = "SELECT MAX(ID) FROM ";
     private static final String SQL_QUERY_CREATE_TOPIC = "INSERT INTO topic (name) VALUES (?)";
     private static final String SQL_QUERY_TOPIC_ID_FROM_QUESTION_TABLE = "SELECT topic_ID FROM question WHERE content=?";
+    private static final String SQL_UPDATE_QUESTION_TO_EXISTING_TOPIC = "UPDATE question SET content=?, difficulty=? WHERE ID=?";
+    private static final String SQL_DELETE_ANSWERS_OF_EXISTING_QUESTION = "DELETE FROM response WHERE question_ID=?";
+    //private static final String SQL_TRIGGER_TO_ADD_QUESTION_AND_ANSWER = "DELIMITER ^_^ CREATE TRIGGER trig BEFORE INSERT ON response FOR EACH ROW BEGIN INSERT INTO question(content, difficulty, topic_ID) values (?,?,?); END^_^";
+    private static final String SQL_QUERY_TO_DELETE_QUESTION = "DELETE FROM question WHERE ID=?";
 
     public List<Question> searchQuestion(String topic) throws SQLException {
         int track = 0;
@@ -71,6 +72,64 @@ public class DaoQuestion extends DatabaseConnection implements DaoQuestionI {
         }
         return result_question;
     }
+    public int deleteQuestion (String topic, String question) throws SQLException {
+
+        //int topic_ID;
+        int question_ID;
+        int verify;
+
+        /*SearchTopicIDbyTopicName search_topic_id = new SearchTopicIDbyTopicName();
+        topic_ID = search_topic_id.searchTopicIDbyTopicName(topic);*/
+        SearchQuestionIDbyQuestionName search_question_id = new SearchQuestionIDbyQuestionName();
+        question_ID = search_question_id.searchQuestionIdByQuestionName(question);
+        System.out.println("QUESTION ID IS "+question_ID);
+
+        if (/*topic_ID == 0 || */question_ID == 0) {
+            //if (question_ID == 0) {
+            System.out.println("No such a question...\n");
+        }
+        //}
+       /* else if (question_ID > 0) {
+            return 0;
+        }*/
+        //   System.err.println(question_ID);
+        //   Question result_question;
+        Connection connect = null;
+        PreparedStatement def_query;
+        PreparedStatement def_query2;
+
+        try {
+            connect = DatabaseConnection.getConnectionToDb();
+            if (connect == null) {
+                //System.err.println("\nError occurred inside Config class --> return_property method\n");
+                return 2;
+            }
+            connect.setAutoCommit(false);
+            def_query = connect.prepareStatement(SQL_QUERY_TO_DELETE_QUESTION);
+            //plug topic into '?' of SQL_QUERY_SEARCH...
+
+            def_query.setInt(1, question_ID);
+
+            // System.out.println();
+            //ResultSet data = def_query.executeUpdate();
+            def_query.executeUpdate();
+
+            /*def_query2 = connect.prepareStatement(SQL_QUERY_SAVE_ANSWER_TO_GIVEN_QUESTION);
+            for (Response answer:answers
+                 ) {
+                def_query2.setString(1,answer.getAnswer());
+                def_query2.setInt(2, question_ID);
+                //TODO: Add UPDATE CASCADE to ALL FOREIGN KEYS IN SQL
+                def_query2.executeQuery();
+            }*/
+            connect.commit();
+        } catch (Exception q) {
+            connect.rollback();
+            q.printStackTrace();
+        }
+
+        return 3;
+    }
 
     public int saveQuestion (String question, int difficulty, String topic, List<Response> answers, int topic_ID) throws SQLException {
 
@@ -105,7 +164,7 @@ public class DaoQuestion extends DatabaseConnection implements DaoQuestionI {
      //   Question result_question;
         Connection connect = null;
         PreparedStatement def_query;
-        PreparedStatement def_query2 = null;
+        PreparedStatement def_query2;
 
         try {
             connect = DatabaseConnection.getConnectionToDb();
@@ -116,24 +175,111 @@ public class DaoQuestion extends DatabaseConnection implements DaoQuestionI {
             connect.setAutoCommit(false);
             def_query = connect.prepareStatement(SQL_QUERY_SAVE_QUESTION_TO_EXISTING_TOPIC);
             //plug topic into '?' of SQL_QUERY_SEARCH...
+
             def_query.setString(1, question);
+
             def_query.setInt(2, difficulty);
+
             def_query.setInt(3, topic_ID);
+           // System.out.println();
             //ResultSet data = def_query.executeUpdate();
             def_query.executeUpdate();
-            def_query2 = connect.prepareStatement(SQL_QUERY_SAVE_ANSWER_TO_GIVEN_QUESTION);
+
+            /*def_query2 = connect.prepareStatement(SQL_QUERY_SAVE_ANSWER_TO_GIVEN_QUESTION);
             for (Response answer:answers
                  ) {
                 def_query2.setString(1,answer.getAnswer());
                 def_query2.setInt(2, question_ID);
                 //TODO: Add UPDATE CASCADE to ALL FOREIGN KEYS IN SQL
                 def_query2.executeQuery();
-            }
+            }*/
             connect.commit();
         } catch (Exception q) {
             connect.rollback();
             q.printStackTrace();
         }
+        AddResponse response = new AddResponse();
+        response.addResponse(SQL_QUERY_SAVE_ANSWER_TO_GIVEN_QUESTION,answers,question_ID);
+        return 3;
+    }
+    public int updateQuestion (String topic, int topic_ID, String old_question, String new_content, int difficulty,  List<Response> answers) throws SQLException {
+
+        //int topic_ID;
+       int question_ID;
+        int verify;
+
+        /*SearchTopicIDbyTopicName search_topic_id = new SearchTopicIDbyTopicName();
+        topic_ID = search_topic_id.searchTopicIDbyTopicName(topic);*/
+        /*SearchQuestionIDbyQuestionName search_question_id = new SearchQuestionIDbyQuestionName();
+        question_ID = search_question_id.searchQuestionIdByQuestionName(old_question);*/
+        VerifyQuestionDuplicate verification = new VerifyQuestionDuplicate();
+        verify = verification.verifyQuestionDuplicate(topic,old_question);
+        if (verify == 5) {
+            System.err.println("Question, that doesn't exist, cannot be updated...");
+            return 0;
+        }
+        SearchQuestionIDbyQuestionName search_question_id = new SearchQuestionIDbyQuestionName();
+        question_ID = search_question_id.searchQuestionIdByQuestionName(old_question);
+       // System.out.println("QUESTION ID IS "+question_ID);
+
+       /* if question_ID == 0) {
+            {
+            GetUnusedId new_q_id = new GetUnusedId();
+            question_ID = new_q_id.getUnusedId("question") + 1;
+        } else {
+            VerifyQuestionDuplicate verification = new VerifyQuestionDuplicate();
+            verify = verification.verifyQuestionDuplicate(topic,question);
+            if (verify == 3) {
+                System.out.println("LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL\n");
+                return 0;
+            }
+            //return 1;
+        }*/
+        //}
+
+        Connection connect = null;
+        PreparedStatement def_query;
+        PreparedStatement def_query2;
+        PreparedStatement def_query3;
+
+        try {
+            connect = DatabaseConnection.getConnectionToDb();
+            if (connect == null) {
+                //System.err.println("\nError occurred inside Config class --> return_property method\n");
+                return 2;
+            }
+            connect.setAutoCommit(false);
+            def_query = connect.prepareStatement(SQL_UPDATE_QUESTION_TO_EXISTING_TOPIC);
+            //plug topic into '?' of SQL_QUERY_SEARCH...
+            def_query.setString(1, new_content);
+            def_query.setInt(2, difficulty);
+            def_query.setInt(3, question_ID);
+            //ResultSet data = def_query.executeUpdate();
+            def_query.executeUpdate();
+
+            //Just deletion
+
+           /* def_query2 = connect.prepareStatement(SQL_DELETE_ANSWERS_OF_EXISTING_QUESTION);
+            def_query2.setInt(1, question_ID);
+            def_query2.executeUpdate();*/
+
+            /*def_query3 = connect.prepareStatement(SQL_QUERY_SAVE_ANSWER_TO_GIVEN_QUESTION);
+            for (Response answer:answers
+            ) {
+                def_query3.setString(1,answer.getAnswer());
+                def_query3.setInt(2, question_ID);
+                //TODO: Add UPDATE CASCADE to ALL FOREIGN KEYS IN SQL
+                def_query3.executeQuery();
+            }*/
+            connect.commit();
+        } catch (Exception q) {
+            connect.rollback();
+            q.printStackTrace();
+        }
+        DeleteResponse del = new DeleteResponse();
+        del.deleteResponse(SQL_DELETE_ANSWERS_OF_EXISTING_QUESTION, question_ID);
+        AddResponse ad = new AddResponse();
+        ad.addResponse(SQL_QUERY_SAVE_ANSWER_TO_GIVEN_QUESTION,answers,question_ID);
         return 3;
     }
     public int createTopic (String topic) throws SQLException {
