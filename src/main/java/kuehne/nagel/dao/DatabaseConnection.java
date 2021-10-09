36585b216a5d674a691602b40f4ab9019e480fc3
ojacomarket <elfutbolesmivida@mail.ru>
2021-10-09 14:55:34 +0300

    FINAL VERSION OF database connection file TRUEEEE

--- src/main/java/kuehne/nagel/dao/DatabaseConnection.java
+++ src/main/java/kuehne/nagel/dao/DatabaseConnection.java
@@ -1,21 +1,30 @@
 package kuehne.nagel.dao;
 
-import kuehne.nagel.config.Config;
 import kuehne.nagel.exceptions.ApplicationPropertiesException;
 import kuehne.nagel.exceptions.CannotReachDatabaseException;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 abstract public class DatabaseConnection {
+
+    public static final String DATABASE_URL = "db.url";
+    public static final String DATABASE_USER = "db.user";
+    public static final String DATABASE_PASSWORD = "db.password";
+    private static final Properties properties = new Properties();
+
     /**
      * Get connection to the database
+     *
      * @param initialTransaction 0 if connection for the first time within SQL QUERY, 1 if not for the first time
      * @return null, if (initalTransaction < 0 || > 1), otherwise connection to the database returned
      * @throws ApplicationPropertiesException if error in application.properties file
-     * @throws CannotReachDatabaseException can caused by many factors, most common are: database is down or database privileges
+     * @throws CannotReachDatabaseException   can caused by many factors, most common are: database is down or database privileges
      */
     public static Connection getConnectionToDb(int initialTransaction) throws ApplicationPropertiesException, CannotReachDatabaseException {
         if (initialTransaction > 1 || initialTransaction < 0) {
@@ -24,9 +33,9 @@ abstract public class DatabaseConnection {
         Connection attemptToConnect;
         try {
             attemptToConnect = DriverManager.getConnection(
-                    Config.return_property(Config.DATABASE_URL),
-                    Config.return_property(Config.DATABASE_USER),
-                    Config.return_property(Config.DATABASE_PASSWORD));
+                    return_property(DATABASE_URL),
+                    return_property(DATABASE_USER),
+                    return_property(DATABASE_PASSWORD));
             if (initialTransaction == 0) {
                 System.out.println("\t\t\t>>> Successfully connected to the database!\n");
             }
@@ -36,5 +45,19 @@ abstract public class DatabaseConnection {
             throw new CannotReachDatabaseException();
         }
         return attemptToConnect;
+    }
+
+    //Read application.properties file, and store its data inside Properties object, which then
+    //is queried by DriverManager class
+    private synchronized static String return_property(String property_name) throws IOException {
+        if (properties.isEmpty()) {
+            try (
+                    BufferedInputStream inputStream = new BufferedInputStream(
+                            new FileInputStream("src/main/resources/application.properties"))
+            ) {
+                properties.load(inputStream);
+            }
+        }
+        return properties.getProperty(property_name);
     }
 }
